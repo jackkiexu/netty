ab637b0eccf7b3c96a6986aaeca31cd4b6706c20
Norman Maurer <norman.maurer@googlemail.com>
2016-12-12 22:25:54 +0100

    correctly not include compression overhead when calculate max record size

--- handler/src/main/java/io/netty/handler/ssl/ReferenceCountedOpenSslEngine.java
+++ handler/src/main/java/io/netty/handler/ssl/ReferenceCountedOpenSslEngine.java
@@ -157,13 +157,11 @@ public class ReferenceCountedOpenSslEngine extends SSLEngine implements Referenc
     }
 
     private static final int MAX_PLAINTEXT_LENGTH = 16 * 1024; // 2^14
-    private static final int MAX_COMPRESSED_LENGTH = MAX_PLAINTEXT_LENGTH + 1024;
-    private static final int MAX_CIPHERTEXT_LENGTH = MAX_COMPRESSED_LENGTH + 1024;
+    private static final int MAX_CIPHERTEXT_LENGTH = MAX_PLAINTEXT_LENGTH + 1024;
 
-    // Header (5) + Data (2^14) + Compression (1024) + Encryption (1024) + MAC (20) + Padding (256)
+    // Header (5) + Data (2^14) + Encryption (1024) + MAC (20) + Padding (256)
+    // No compression supported!
     static final int MAX_ENCRYPTED_PACKET_LENGTH = MAX_CIPHERTEXT_LENGTH + 5 + 20 + 256;
-
-    static final int MAX_ENCRYPTION_OVERHEAD_LENGTH = MAX_ENCRYPTED_PACKET_LENGTH - MAX_PLAINTEXT_LENGTH;
 
     private static final AtomicIntegerFieldUpdater<ReferenceCountedOpenSslEngine> DESTROYED_UPDATER;
 
